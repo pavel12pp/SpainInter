9ca30c09ad7231e7f0d91e5f6e7f023cc0daa413
Pavel Komandyshko <pavelkom12@yandex.ru>
2024-11-19 07:17:50 +0300

    Gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 require("./gulp/dev.js");
 require("./gulp/prod.js");
+require("./gulp/deploy.js");
 
 gulp.task('default', gulp.series(
   "clean:dev",
@@ -12,4 +13,9 @@ gulp.task('prod', gulp.series(
   "clean:prod",
   gulp.parallel("template:prod", "style:prod", "asset:prod", "script:prod"),
   "server:prod"
+));
+
+gulp.task('deploy', gulp.series(
+  "clean:deploy",
+  gulp.parallel("template:deploy", "style:deploy", "asset:deploy", "script:deploy"),
 ));
